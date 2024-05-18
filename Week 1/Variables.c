977f90635daee12efe7304c3cfd4f99db1e65c00
thatjordandude <jordanians@yahoo.com>
2024-05-18 18:20:21 -0500

    More week 1

--- Week 1/Variables.c
+++ Week 1/Variables.c
@@ -3,7 +3,21 @@
 
 int main()
 {
-    
+    printf("George was 40\nGeorge liked to play baseball\nGeorge had a dog named Toby\nToby was a good and happy dog\nGeorge liked his name George\n\n");
+
+    //Variables:
+    char character_name[] = "John";
+    char dog_name[] = "Sport";
+    int character_age = 25;
+
+
+
+    printf("%s was %d\n%s liked to play baseball\n%s had a dog named Toby\n"
+    ,character_name,character_age,character_name,character_name);
+
+    char character_name2[] = "George";
+    printf("%s was a good and happy dog\n%s liked his name %s\n"
+    ,dog_name,character_name2,character_name2);
 
     return 0;
 }
---
+++ Week 1/data_types.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+
+int main()
+{
+    int age = 40;
+    
+    //decimal numbers:
+
+    //double
+    double gpa = 3.4;
+
+    //float
+    float weight = 129.99;
+
+    //Character - can only store 1 single character (unless has [] which is a string)
+    char grade = "D";
+
+    //String   technically an array
+    char name[] = "Brad"; //"special" compared to other data sets
+
+
+
+
+
+
+
+
+    return 0;
+}
