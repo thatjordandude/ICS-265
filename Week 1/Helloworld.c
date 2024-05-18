a91867943c0490dceeecca28d1a949d7ad2efad8
thatjordandude <jordanians@yahoo.com>
2024-05-18 12:35:23 -0500

    Beginning learning

---
+++ Week 1/Helloworld.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+// comment line
+/*The code below will print the words Hello World!
+to the screen, and it is amazing*/
+int main() {
+int age = 24;
+  printf("Hello World!\n");
+  printf("My age is %d",age);
+  printf("New line \nNew line\n");
+  printf("\n\n\n");
+  printf("\thorizontal tab");
+  return 0;
+}
